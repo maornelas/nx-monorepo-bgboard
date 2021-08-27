9b6179e93185ac6a9afc8badbf00deca59c932f9
Marco Ornelas <maornelasr@gmail.com>
2021-08-27 14:31:27 -0500

    Set up the Board Game Hoard API - Nx Workspaces

--- libs/api-interfaces/src/lib/api-interfaces.ts
+++ libs/api-interfaces/src/lib/api-interfaces.ts
@@ -1,3 +1,19 @@
-export interface Message {
-  message: string;
+export interface Game {
+  name: string;
+  id: string;
+  image?: string;
+  description: string;
+  price: number;
+  rating?: number;
+  reviews?: Array<Review>;
+}
+export interface Review {
+  game: string;
+  rating: number;
+  content: string;
+}
+
+export interface CartItem {
+  game: string;
+  quantity: number;
 }
